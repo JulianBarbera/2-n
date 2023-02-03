80359b44495d14252c7e2b2a51ac221d8293eb6b
JulianBarbera <77413091+JulianBarbera@users.noreply.github.com>
2023-02-03 10:46:40 -0800

    Added argparse

--- main.cpp
+++ main.cpp
@@ -1,8 +1,36 @@
 #include "File_man.h"
 #include "SDL_io.h"
+#include <args.hxx>
 #include <cstdint>
 
-int main(int argc, char *args[]) {
+int main(int argc, char **args) {
+  args::ArgumentParser parser("2n - A very large 2048 clone",
+                              "2n Dev by Julian Barbera");
+  args::HelpFlag help(parser, "help", "Display this help menue", {'h', "flag"});
+  args::ValueFlag<uint8_t> newGame(
+      parser, "size", "Makes a new game with a specific board size",
+      {'n', "new"});
+  args::CompletionFlag completion(parser, {"complete"});
+
+  try {
+    parser.ParseCLI(argc, args);
+  } catch (args::Help) {
+    std::cout << parser;
+  } catch (args::ParseError e) {
+    std::cerr << e.what() << std::endl;
+    std::cerr << parser;
+    return 1;
+  } catch (args::ValidationError e) {
+    std::cerr << e.what() << std::endl;
+    std::cerr << parser;
+    return 1;
+  }
+
+  if (newGame) {
+    cout << "New game with size " << args::get(newGame) << endl;
+    system("rm 2nData.dat");
+  }
+
   SDL_io SDL_io_o;
   return SDL_io_o.run();
   return 0;
